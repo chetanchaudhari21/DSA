d6258bb3fc199873ec57fc15be00821d90055a63
Chetan Chaudhari <chetanchaudhari9c@gmail.com>
2026-08-16 20:07:12 +0530

    Time: 14 ms (58.06%), Space: 46.2 MB (82.23%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,32 +1,39 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int freq[]=new int[256];
 
-        int low=0;
-        int high=0;
-        int charmax=0;
-        int res=0;
-        int freqlen=0;
-        int diff=0;
-        
-        while(high<answerKey.length())
-        {
-            freq[answerKey.charAt(high)-'A']++;
-            charmax=Math.max(charmax,freq[answerKey.charAt(high)-'A']);
-            diff=(high-low+1)-charmax;
+        int freq[] = new int[2];
 
-            while(diff>k)
-            {
-                freq[answerKey.charAt(low)-'A']--;
+        int low = 0;
+        int high = 0;
+        int charmax = 0;
+        int res = 0;
+
+        while(high < answerKey.length()) {
+
+            int index = answerKey.charAt(high) == 'T' ? 0 : 1;
+
+            freq[index]++;
+
+            charmax = Math.max(charmax, freq[index]);
+
+            int diff = (high - low + 1) - charmax;
+
+            while(diff > k) {
+
+                int remove = answerKey.charAt(low) == 'T' ? 0 : 1;
+
+                freq[remove]--;
+
                 low++;
-                diff=(high-low+1)-charmax;
+
+                diff = (high - low + 1) - charmax;
             }
 
-            res=Math.max(res,high-low+1);
+            res = Math.max(res, high - low + 1);
+
             high++;
-
-
         }
+
         return res;
     }
 }
