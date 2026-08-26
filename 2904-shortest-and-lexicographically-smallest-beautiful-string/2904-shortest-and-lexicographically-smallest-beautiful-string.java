ae67a1f6c1f66f62563300db01a7232f330dce72
Chetan Chaudhari <chetanchaudhari9c@gmail.com>
2026-08-26 15:31:16 +0530

    Time: 1 ms (100%), Space: 43.8 MB (87.25%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,50 +1,47 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        int low=0;
-        int len;
-        int result=s.length();
-        int count=0;
-        int start=0;
-        boolean flag=false;
-        for(int high=0;high<s.length();high++)
-        {
-            if(s.charAt(high)=='1')
-            {
+        int low = 0;
+        int count = 0;
+        int result = s.length() + 1;
+        int start = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '1') {
                 count++;
             }
-            while(count==k)
-            {
-                flag=true;
-                len=high-low+1;
-                if(result>len)
-                {
-                    start=low;
-                    result=len;
-                }
-                if(result==len)
-                {
-                    String previous=s.substring(start,start+result);
-                    String current=s.substring(low,high+1);
 
-                    if(current.compareTo(previous)<0)
-                    {
-                        start=low;
+            while (count == k) {
+                flag = true;
+
+                int len = i - low + 1;
+
+                if (result > len) {
+                    start = low;
+                    result = len;
+                } 
+                else if (result == len) {
+                    String current = s.substring(low, i + 1);
+                    String previous = s.substring(start, start + result);
+
+                    if (current.compareTo(previous) < 0) {
+                        start = low;
                     }
-
                 }
-                if(s.charAt(low)=='1')
-                {
+
+                if (s.charAt(low) == '1') {
                     count--;
                 }
+
                 low++;
             }
         }
-        if(flag)
-        {
-            return s.substring(start,start+result);
+
+        if (flag) {
+            return s.substring(start, start + result);
         }
+
         return "";
-
-
     }
 }
