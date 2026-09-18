9c54bb184b344288a85e526567ce3d989c3f7b63
Chetan Chaudhari <chetanchaudhari9c@gmail.com>
2026-09-18 17:30:05 +0530

    Time: 6 ms (5.31%), Space: 43.4 MB (40.76%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> stack  = new Stack<>(); 
+       int i=0;
+
+        
+       for(i=0;i<s.length();i++)
+       {
+        
+        char current=s.charAt(i);
+        if(stack.empty())
+        {
+            stack.push(s.charAt(i));
+            
+        }
+        
+        else if((stack.peek()=='{' && current=='}') || (stack.peek()=='[' && current== ']') || (stack.peek()=='(' && current==')'))
+        {
+            stack.pop();
+        }
+        else
+        {
+            stack.push(s.charAt(i));
+        }
+        
+
+       }
+       if(stack.empty())
+       {
+        return true;
+       }
+       return false;
+    }
+}
