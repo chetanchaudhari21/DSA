ed6018122b6c5b9dba66a31bb05dcd5115ba3e53
Chetan Chaudhari <chetanchaudhari9c@gmail.com>
2026-09-19 16:29:19 +0530

    Time: 31 ms (41.24%), Space: 47.3 MB (46.87%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -5,8 +5,9 @@ class Solution {
 
         for(int i = 0; i < s.length(); i++) {
 
+            char ch = s.charAt(i);
 
-            if(!stack.empty() && stack.peek()[0] == s.charAt(i)) {
+            if(!stack.empty() && stack.peek()[0] == ch) {
 
                 stack.peek()[1]++;
 
@@ -16,7 +17,7 @@ class Solution {
 
             } else {
 
-                stack.push(new int[]{s.charAt(i), 1});
+                stack.push(new int[]{ch, 1});
             }
         }
 
